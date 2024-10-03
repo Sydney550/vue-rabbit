16ffabc369e39d35a2523ec3ced95ea844069b6a
Sydney550 <1614903064@qq.com>
2024-10-03 20:10:23 +0800

    pinia优化导航数据

---
+++ src/stores/category.js
@@ -0,0 +1,16 @@
+import { ref } from 'vue'
+import { getCategoryAPI } from '@/apis/layout'
+import { defineStore } from 'pinia'
+
+export const useCategoryStore = defineStore('category', () => {
+    const categoryList = ref([])
+    const getCategory = async () => {
+      const res = await getCategoryAPI()
+      categoryList.value = res.result
+    }
+
+  return { 
+    categoryList,
+    getCategory 
+  }
+})
