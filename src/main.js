acaf4a09e38d82be0aa4168b0e3ffd3a6b7898f5
Sydney550 <1614903064@qq.com>
2024-10-08 21:02:21 +0800

    图片懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
 
 import App from './App.vue'
 import router from './router'
@@ -13,3 +16,20 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        // el: 指令所绑定的元素，img
+        // binding: bingding.value是指令“=”后面的值，即图片的src
+        const {stop} = useIntersectionObserver(
+            el,
+            ([{isIntersecting}]) => {
+                if (isIntersecting) {
+                    // 进入可视区域，加载图片
+                    el.src = binding.value
+                    stop()
+                }
+            }
+        )
+    }
+})
