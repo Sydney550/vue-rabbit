1cc03f8fbab4f3929ed4792792159280de21df23
Sydney550 <1614903064@qq.com>
2024-10-14 20:50:07 +0800

    详情页-全局组件插件化

--- src/main.js
+++ src/main.js
@@ -5,13 +5,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 引入初始化样式
 import '@/styles/common.scss'
 
+// 引入懒加载指令插件并注册
 import { lazyPlugin } from '@/directives'
+
+// 引入全局组件插件
+import { componentPlugin } from '@/components'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
